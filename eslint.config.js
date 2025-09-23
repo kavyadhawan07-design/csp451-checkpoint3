16b347fb0f118c58254b0c66924f298b791c416c
Kavya Dhawan <kdhawan10@myseneca.ca>
2025-09-23 14:02:04 -0400

    Prettier final fix: ensure all files match formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,8 @@
-import prettierPlugin from 'eslint-plugin-prettier';
-import prettierConfig from 'eslint-config-prettier';
-
 export default [
   {
-    ignores: ['dist/', 'node_modules/'],
-  },
-  {
-    files: ['**/*.js'],
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
-    },
     rules: {
-      'no-unused-vars': 'warn',
-    },
-  },
-  {
-    plugins: {
-      prettier: prettierPlugin,
-    },
-    rules: {
-      ...prettierConfig.rules,
-      'prettier/prettier': 'error',
+      semi: 'error',
+      quotes: ['error', 'single'],
     },
   },
 ];
