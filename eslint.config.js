e38780284f7386b974301d3c9bed81299b6c3982
Kavya Dhawan <kdhawan10@myseneca.ca>
2025-09-23 14:21:03 -0400

    Manually fix ESLint and Prettier issues

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,12 @@
-export default [
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
   {
+    files: ["**/*.js"],
     rules: {
-      semi: 'error',
-      quotes: ['error', 'single'],
-    },
-  },
-];
+      semi: "error",
+      "prefer-const": "error",
+      "no-unused-vars": "warn"
+    }
+  }
+]);
