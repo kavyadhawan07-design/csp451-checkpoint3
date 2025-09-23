a4381b26973918393d041d424879b39ca7b6e469
Kavya Dhawan <kdhawan10@myseneca.ca>
2025-09-23 14:10:57 -0400

    Fix ESLint single-quote violations

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
-import fs from "fs";
+import fs from 'fs';
 
-fs.mkdirSync("build", { recursive: true });
-fs.writeFileSync("build/index.html", "<h1>Hello CI/CD</h1>");
-console.log("Building application...");
+fs.mkdirSync('build', { recursive: true });
+fs.writeFileSync('build/index.html', '<h1>Hello CI/CD</h1>');
+console.log('Building application...');
