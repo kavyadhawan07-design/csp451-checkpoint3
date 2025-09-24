6eef8784620a36c709ee5e6aa5579e93f996e16c
Kavya Dhawan <kdhawan10@myseneca.ca>
2025-09-24 14:50:25 -0400

    style: fix formatting and lint issues

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ export default defineConfig([
     rules: {
       semi: "error",
       "prefer-const": "error",
-      "no-unused-vars": "warn"
-    }
-  }
+      "no-unused-vars": "warn",
+    },
+  },
 ]);
